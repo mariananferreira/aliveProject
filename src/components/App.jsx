5f099d6697b68c50d1992531e9d12c92ca14f343
Mariana Ferreira <62514533+mariananferreira@users.noreply.github.com>
2023-06-02 14:39:06 +0100

    changed divs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Cropper from 'react-cropper'
 import 'cropperjs/dist/cropper.css'
+import { Container } from '@/components/Container'
 
 export function App() {
   const defaultSrc =
@@ -31,47 +32,51 @@ export function App() {
   }
 
   return (
-    <div>
-      <div className="splitdiv" id="leftdiv">
-        <div id="leftdivcard">
-          <input type="file" onChange={onChange} />
-          &nbsp; &nbsp;
-          <button
-            onClick={getCropData}
-            className=" mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent px-6 py-3 text-base font-medium bg-emerald-500 text-white shadow-sm hover:bg-emerald-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-            type="button"
-            id="leftbutton"
-          >
-            Crop Image
-          </button>
-          <Cropper
-            className="cropper"
-            zoomTo={0.5}
-            initialAspectRatio={1}
-            src={image}
-            viewMode={1}
-            minCropBoxHeight={10}
-            minCropBoxWidth={10}
-            background={false}
-            responsive={true}
-            autoCropArea={1}
-            checkOrientation={false}
-            onInitialized={(instance) => {
-              setCropper(instance)
-            }}
-            guides={true}
-          />
-        </div>
-      </div>
-      <div className="splitdiv" id="rightdiv">
-        <div id="itemdivcard">
-          {cropData ? (
-            <img style={{  }} src={cropData} alt="cropped" />
-          ) : (
-            <h1>Cropped image will apear here</h1>
-          )}{' '}
-        </div>
-      </div>
-    </div>
+      <section id="App" aria-label="" className="lg:pb-24">
+        <Container>
+          <div className="splitdiv" id="leftdiv">
+            <div id="leftdivcard">
+              <input type="file" onChange={onChange} />
+              &nbsp; &nbsp;
+              <button
+                onClick={getCropData}
+                className="my-2	mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-emerald-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-emerald-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                type="button"
+                id="leftbutton"
+              >
+                Crop Image
+              </button>
+              <Cropper
+                className="cropper"
+                zoomTo={0.5}
+                initialAspectRatio={1}
+                src={image}
+                viewMode={1}
+                minCropBoxHeight={10}
+                minCropBoxWidth={10}
+                background={false}
+                responsive={true}
+                autoCropArea={1}
+                checkOrientation={false}
+                onInitialized={(instance) => {
+                  setCropper(instance)
+                }}
+                guides={true}
+              />
+            </div>
+          </div>
+          <div className="splitdiv" id="rightdiv">
+            <div id="itemdivcard">
+              {cropData ? (
+                <div className="-m-2 rounded-xl bg-emerald-500 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <img style={{ height: '50%' }} src={cropData} alt="cropped" />{' '}
+                </div>
+              ) : (
+                <h1>Cropped image will apear here</h1>
+              )}{' '}
+            </div>
+          </div>
+        </Container>
+      </section>
   )
 }
